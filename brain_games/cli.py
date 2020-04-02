c9f0256b206cbff08f13a8c4ff3dda4c2261b8cd
Yurii .p <stroitelisf@yandex.ru>
2020-04-02 10:29:25 +0300

    find bug

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+import prompt
+
+
+def say_your_name():
+    name = promt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+
+from brain_games.cli import say_your_name
 def greeting(hello):
     print(hello)
 
 def main():
     greeting('Welcome to the Brain Games!')
+    say_your_name()
 
 if __name__ == '__main__':
     main()
