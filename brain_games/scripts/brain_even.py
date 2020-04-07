be3a77b0f2145445e3d76cd3161a2bbb3ae74893
Yurii .p <stroitelisf@yandex.ru>
2020-04-07 23:53:04 +0300

    fix: long line

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -11,18 +11,19 @@ game_rules = 'Answer "yes" if number even otherwise answer "no".\n'
 count_attempts = 3
 
 
-def logic_even_question(user_name, count_attempts=0):
+def logic_even_question(name, count_attempts=0):
     if count_attempts != 3:
-        corrent_number = random.randint(1, 100)
-        answer = prompt.string(f"Question: {corrent_number}\nYour answer: ")
-        if is_even(corrent_number) == answer:
+        number = random.randint(1, 100)
+        answer = prompt.string(f"Question: {number}\nYour answer: ")
+        if is_even(number) == answer:
             print("Correct!")
             count_attempts += 1
-            logic_even_question(user_name, count_attempts)
+            logic_even_question(name, count_attempts)
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{is_even(corrent_number)}'.\nLet's try again, {user_name}!")
+            print(f"""'{answer}' is wrong answer ;(. Correct answer was '{is_even(number)}'.
+Let's try again, {name}!""")
     else:
-        print(f"Congratulations, {user_name}!")
+        print(f"Congratulations, {name}!")
 
 
 def main():
